df858cf2528ce17577eb1b34c68556b842110e23
keiz1213 <fukuoka00tsc@gmail.com>
2022-12-18 14:40:58 +0900

    イベント一覧機能追加

--- app/controllers/welcom_controller.rb
+++ app/controllers/welcom_controller.rb
@@ -2,5 +2,6 @@ class WelcomController < ApplicationController
   skip_before_action :authenticate
 
   def index
+    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end
